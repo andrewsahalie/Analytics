db46a5310b8c171edf184a42c0d2181506966407
Andrew Sahalie <andrewsahalie@yahoo.com>
2016-06-17 12:50:32 -0700

    Update Analytics 04 Time.R
    Formatted a little

--- Analytics 04 Time.R
+++ Analytics 04 Time.R
@@ -1,4 +1,4 @@
-#Analytics Data
+# Analytics Data
 
 require(plyr)
 require(dplyr)
@@ -19,58 +19,77 @@ Analytics$DateTest <- strptime(Analytics$Date, "%Y-%m-%d %H:%M:%S")
 Analytics$Minute <- substr(Analytics$DateTest, 1, 16)
 
 require(dplyr)
-MinutesUserLesson <-  subset(Analytics[Analytics$Environment=="Hillock" | Analytics$Environment=="Lecture view" | Analytics$Environment=="Neuron" | Analytics$Environment=="SynapticCleft",], select=-c(DateTest))
+MinutesUserLesson <-  subset(Analytics[
+	Analytics$Environment == "Hillock" |
+	Analytics$Environment == "Lecture view" |
+	Analytics$Environment == "Neuron" |
+	Analytics$Environment == "SynapticCleft",
+], select = -c(DateTest))
 
-MinutesUserLessonSummary <- ddply(MinutesUserLesson, c("UserIDMaybe", "Lesson"), summarise, N = length(unique(Minute)), ActionCount = length(unique(UniqueID)))
+MinutesUserLessonSummary <- ddply(MinutesUserLesson,
+	c("UserIDMaybe", "Lesson"), summarise, MinutesUsed = length(unique(Minute)), ActionCount = length(unique(UniqueID)))
+
 # Don’t count the first minute - not a full minute:
-MinutesUserLessonSummary$N <- MinutesUserLessonSummary$N-1
+MinutesUserLessonSummary$MinutesUsed <- MinutesUserLessonSummary$MinutesUsed - 1
 
 # Minutes used per lesson
-# Creating a Minute variable and counting unique instances of minutes per environment [ONLY BRIEFLY TESTED]
+# Creating a Minute variable and counting unique instances of minutes per environment
+# Only includes those users who performed at least 20 actions as per Y1RFS report.
 require(dplyr)
-MinutesLessonSummary <- ddply(MinutesUserLessonSummary[MinutesUserLessonSummary$ActionCount>19,], c("Lesson"), summarise, UserMinutes = sum(N), MeanUserMinutes = mean(N))
-
+MinutesLessonSummary <- ddply(MinutesUserLessonSummary[MinutesUserLessonSummary$ActionCount > 19, ],
+	c("Lesson"), summarise, UserMinutes = sum(MinutesUsed), MeanUserMinutes = mean(MinutesUsed))
 
 
 
 # Use this to select only records that happen after a specific time:
-#MinutesAfter <- unique(Minutes[Minutes$Minute>"2016-02-16 11:59",])
+# MinutesAfter <- unique(Minutes[Minutes$Minute>"2016-02-16 11:59", ])
+
 # Use this to test data:
-#Test <- unique(Minutes[Minutes$UserIDMaybe=="27",])
+# Test <- unique(Minutes[Minutes$UserIDMaybe == "27", ])
 
 # Minutes used per student per environment
-# Creating a Minute variable and counting unique instances of minutes per student per environment [ONLY BRIEFLY TESTED]
+# Creating a Minute variable and counting unique instances of minutes per student per environment
 Analytics$DateTest <- strptime(Analytics$Date, "%Y-%m-%d %H:%M:%S")
 Analytics$Minute <- substr(Analytics$DateTest, 1, 16)
 
 require(dplyr)
-MinutesUserEnvironment <-  subset(Analytics[Analytics$Environment=="Hillock" | Analytics$Environment=="Lecture view" | Analytics$Environment=="Neuron" | Analytics$Environment=="SynapticCleft",], select=-c(DateTest))
 
-MinutesUserEnvironmentSummary <- ddply(MinutesUserEnvironment, c("UserIDMaybe", "Environment"), summarise, N = length(unique(Minute)), ActionCount = length(unique(UniqueID)))
+MinutesUserEnvironment <-
+subset(Analytics[
+	Analytics$Environment == "Hillock" |
+	Analytics$Environment == "Lecture view" |
+	Analytics$Environment == "Neuron" |
+	Analytics$Environment == "SynapticCleft",
+], select = -c(DateTest))
+
+MinutesUserEnvironmentSummary <- ddply(MinutesUserEnvironment,
+	c("UserIDMaybe", "Environment"), summarise, MinutesUsed = length(unique(Minute)), ActionCount = length(unique(UniqueID)))
+
 # Don’t count the first minute - not a full minute:
-MinutesUserEnvironmentSummary$N <- MinutesUserEnvironmentSummary$N-1
+MinutesUserEnvironmentSummary$MinutesUsed <- MinutesUserEnvironmentSummary$MinutesUsed - 1
 
 # Minutes used per environment
-# Creating a Minute variable and counting unique instances of minutes per environment [ONLY BRIEFLY TESTED]
+# Creating a Minute variable and counting unique instances of minutes per environment
 # Only includes those users who performed at least 20 actions as per Y1RFS report.
 require(dplyr)
-MinutesEnvironmentSummary <- ddply(MinutesUserEnvironmentSummary[MinutesUserEnvironmentSummary$ActionCount>19,], c("Environment"), summarise, UserMinutes = sum(N), AvgUserMinutes = mean(N), UserActions = sum(ActionCount), AvgUserActions = mean(ActionCount))
+MinutesEnvironmentSummary <- ddply(MinutesUserEnvironmentSummary[MinutesUserEnvironmentSummary$ActionCount > 19,],
+	c("Environment"), summarise, UserMinutes = sum(MinutesUsed), AvgUserMinutes = mean(MinutesUsed), UserActions = sum(ActionCount),
+	AvgUserActions = mean(ActionCount))
 
-MinutesUserLessonSummary$MinBin <- cut(MinutesUserLessonSummary$N, breaks = seq(-5, 120, by = 5))
+MinutesUserLessonSummary$MinBin <- cut(MinutesUserLessonSummary$MinutesUsed, breaks = seq(0, 120, by = 5), include.lowest = TRUE)
 MinutesUserLessonBins <- ddply(MinutesUserLessonSummary, c("MinBin"), summarise, Users = length(unique(UserIDMaybe)))
 
 
-#MinutesUserLessonBinsOOC <-  subset(MinutesUserLessonBins, select=-c(DateTest))
-MinutesUserLessonBinsOOC <- ddply(MinutesUserLessonSummary[MinutesUserLessonSummary$Lesson == "undefined",], c("MinBin"), summarise, Users = length(unique(UserIDMaybe)))
-
-
+# MinutesUserLessonBinsOOC <-  subset(MinutesUserLessonBins, select = -c(DateTest))
+MinutesUserLessonBinsOOC <- ddply(MinutesUserLessonSummary[MinutesUserLessonSummary$Lesson == "undefined", ],
+	c("MinBin"), summarise, Users = length(unique(UserIDMaybe)))
 
 
 
 # Use this to select only records that happen after a specific time:
-#MinutesEnvironmentAfter <- unique(MinutesEnvironment[MinutesEnvironment$Minute>"2016-02-16 11:59",])
+# MinutesEnvironmentAfter <- unique(MinutesEnvironment[MinutesEnvironment$Minute > "2016-02-16 11:59", ])
 # Use this to test data:
-#Test <- unique(MinutesEnvironment[MinutesEnvironment$UserIDMaybe=="27",])
+# Test <- unique(MinutesEnvironment[MinutesEnvironment$UserIDMaybe == "27", ])
 
 
 
