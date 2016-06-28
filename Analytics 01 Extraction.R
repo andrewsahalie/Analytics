f87e08580b5815f474a9e261f9f4f586acdd9239
Andrew Sahalie <andrewsahalie@yahoo.com>
2016-06-28 08:06:12 -0700

    Regular Update
    Just a regular update to make sure everything is current. No known changes.

--- Analytics 01 Extraction.R
+++ Analytics 01 Extraction.R
@@ -1,5 +1,7 @@
-# Analytics Data
-# EXTRACTION: The real extraction code hasn’t been written yet
+# Analytics Data: A data manual exists for these data. See Andrew.
+# EXTRACTION: The real extraction code (connecting directly to JSON data) hasn’t been written yet - needs final JSON details.
+# This CSV file is the source for now.
+
 # LogsTableRaw<-fromJSON("URLFromCaz")
 
 # Extracting the data
@@ -9,16 +11,16 @@
 
 # Reading in raw data from CSV files. These files are created from https://destinationbiology.com/analytics/ (See documentation about how to get these data elsewhere)
 
-	AnalyticsFeeder01 <- read.csv("~/Documents/Y1RFS/Analytics Data/Raw Data/Y1RFS Day 1 DataFix.csv",  header=FALSE, sep="|", stringsAsFactors=FALSE)
+	AnalyticsFeeder01 <- read.csv("~/Documents/Y1RFS/Analytics Data/Raw Data/Y1RFS Day 1 DataFix.csv",  header = FALSE, sep = "|", stringsAsFactors = FALSE)
 	AnalyticsFeeder01 <- AnalyticsFeeder01[, c("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12")]
 
-	AnalyticsFeeder02 <- read.csv("~/Documents/Y1RFS/Analytics Data/Raw Data/Y1RFS Day-2 Data.csv",  header=FALSE, sep="|", stringsAsFactors=FALSE)
+	AnalyticsFeeder02 <- read.csv("~/Documents/Y1RFS/Analytics Data/Raw Data/Y1RFS Day-2 Data.csv",  header = FALSE, sep = "|", stringsAsFactors = FALSE)
 	AnalyticsFeeder02 <- AnalyticsFeeder02[, c("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12")]
 
-	AnalyticsFeeder03 <- read.csv("~/Documents/Y1RFS/Analytics Data/Raw Data/Y1RFS Day3Data.csv",  header=FALSE, sep="|", stringsAsFactors=FALSE)
+	AnalyticsFeeder03 <- read.csv("~/Documents/Y1RFS/Analytics Data/Raw Data/Y1RFS Day3Data.csv",  header = FALSE, sep = "|", stringsAsFactors = FALSE)
 	AnalyticsFeeder03 <- AnalyticsFeeder03[, c("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12")]
 
-	AnalyticsFeeder04 <- read.csv("~/Documents/Y1RFS/Analytics Data/Raw Data/Y1RFS NonLectureDataFix.csv",  header=FALSE, sep="|", stringsAsFactors=FALSE)
+	AnalyticsFeeder04 <- read.csv("~/Documents/Y1RFS/Analytics Data/Raw Data/Y1RFS NonLectureDataFix.csv",  header = FALSE, sep = "|", stringsAsFactors = FALSE)
 	AnalyticsFeeder04 <- AnalyticsFeeder04[, c("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12")]
 
 #Merges files into a common temporary file. A good place to start when re-running stuff after breaking something.
